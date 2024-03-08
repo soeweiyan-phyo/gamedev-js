845a7b9753be4c70390e5f5c462a3c35854056e9
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2024-03-09 01:25:53 +1100

    Change animations from different player states

--- script.js
+++ script.js
@@ -10,16 +10,92 @@ const spriteHeight = 523;
 
 let frameX = 0;
 let frameY = 0;
-// To control frame rate
-let gameFrame = 0;
-const staggerFrames = 2;
 
+// To control frame rate in x-axis
+let gameFrame = 0;
+const staggerFrames = 3;
+
+// To control different animations in y-axis
+let playerState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function (e) {
+  playerState = e.target.value;
+});
+
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 4,
+  },
+];
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
+
+// Animation
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  // Control frame rate and frameX position
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    spriteAnimations[playerState].loc.length;
+  frameX = spriteWidth * position;
+
+  frameY = spriteAnimations[playerState].loc[position].y;
+
+  // Draw sprite
   ctx.drawImage(
     playerImage,
-    frameX * spriteWidth, // Move along x-axis in sprite sheet
-    frameY * spriteHeight, // Move along y-axis in sprite sheet
+    frameX, // Move along x-axis in sprite sheet
+    frameY, // Move along y-axis in sprite sheet
     spriteWidth,
     spriteHeight,
     0,
@@ -28,14 +104,7 @@ function animate() {
     spriteHeight
   );
 
-  // Control frame rate
-  if (gameFrame % staggerFrames === 0) {
-    // Move frame
-    if (frameX < 6) frameX++;
-    else frameX = 0;
-  }
-
-  gameFrame++;
+  gameFrame++; // Change frame
   requestAnimationFrame(animate);
 }
 
